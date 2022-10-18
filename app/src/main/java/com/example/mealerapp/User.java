a2fa6d50d237984f36e1c3c6a2f12a983c3b79d6
Mohamed Shakir <shakirmohamed253@gmail.com>
2022-10-17 20:04:07 -0400

    Created general User class and its methods.

---
+++ app/src/main/java/com/example/mealerapp/User.java
@@ -0,0 +1,56 @@
+package com.example.mealerapp;
+
+public class User {
+
+    private String firstName, lastName, Address, Email, Password;
+
+    public User (String firstName, String Address, String lastName, String Email, String Password){
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.Address = Address;
+        this.Email = Email;
+        this.Password = Password;
+
+    }
+
+    public void setName(String firstName, String lastName){
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+
+    public String getName(){
+        return this.firstName + " " + this.lastName;
+    }
+
+    public void setAddress (String Address) {
+        this.Address = Address;
+    }
+
+    public String getAddress(){
+        return this.Address;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getEmail(){
+        return this.Email;
+    }
+
+    public void setPassword(String Password){
+        this.Password = Password;
+    }
+
+    public String getPassword(){
+        return this.Password;
+    }
+
+
+
+
+
+}
