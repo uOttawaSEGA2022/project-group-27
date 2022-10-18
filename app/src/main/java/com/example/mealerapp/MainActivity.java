8a731432712dbd02341935fdfaf5fe7c3d2f8d07
Mohamed Shakir <shakirmohamed253@gmail.com>
2022-10-18 09:23:09 -0400

    Began connecting activities and adding onClick Event Listeners

--- app/src/main/java/com/example/mealerapp/MainActivity.java
+++ app/src/main/java/com/example/mealerapp/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.mealerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void onActivityResult(View view){
+
     }
 }
--- app/src/main/java/com/example/mealerapp/SelectUserType.java
+++ app/src/main/java/com/example/mealerapp/SelectUserType.java
@@ -2,6 +2,7 @@ package com.example.mealerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SelectUserType extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class SelectUserType extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_user_type);
+    }
+
+    public void cook(){
+        startActivity(new Intent(this, CookActivity.class));
     }
 }
