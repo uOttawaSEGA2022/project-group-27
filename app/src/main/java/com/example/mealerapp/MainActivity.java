7eba9d8377b44ef492f60f2e7d80d8cacd46a2a8
Mohamed Shakir <mshak084@uottawa.ca>
2022-10-19 16:08:26 -0400

    Updated main activity design, added even listeners for sign up buttons, creating Client Activity.

--- app/src/main/java/com/example/mealerapp/MainActivity.java
+++ app/src/main/java/com/example/mealerapp/MainActivity.java
@@ -5,17 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView signUp;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUp = (TextView)findViewById(R.id.signUp);
+        signUp.setOnClickListener(this);
     }
 
-    protected void onActivityResult(View view){
-        // test
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.signUp:
+                startActivity(new Intent(this, SelectUserType.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/mealerapp/SelectUserType.java
+++ app/src/main/java/com/example/mealerapp/SelectUserType.java
@@ -4,16 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SelectUserType extends AppCompatActivity {
+public class SelectUserType extends AppCompatActivity implements View.OnClickListener{
+
+    private Button cook;
+    private Button client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_user_type);
+
+        cook = (Button) findViewById(R.id.cook);
+        cook.setOnClickListener(this);
+        client = (Button) findViewById(R.id.client);
+        client.setOnClickListener(this);
+
     }
 
-    public void cook(){
-        startActivity(new Intent(this, CookActivity.class));
+
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.cook:
+                startActivity(new Intent(this, CookActivity.class));
+                break;
+            case R.id.client:
+                startActivity(new Intent(this, ClientActivity.class));
+                break;
+        }
     }
 }
