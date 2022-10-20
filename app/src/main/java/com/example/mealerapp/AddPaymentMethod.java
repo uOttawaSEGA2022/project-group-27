eb6799df2b3435b9f74f23461b20716552d29fac
Mohamed Shakir <mshak084@uottawa.ca>
2022-10-20 10:33:30 -0400

    Started adding logic for adding payment method and transferring data between activities when making an account

---
+++ app/src/main/java/com/example/mealerapp/AddPaymentMethod.java
@@ -0,0 +1,14 @@
+package com.example.mealerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPaymentMethod extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_payment_method);
+    }
+}
--- app/src/main/java/com/example/mealerapp/ClientActivity.java
+++ app/src/main/java/com/example/mealerapp/ClientActivity.java
@@ -2,13 +2,45 @@ package com.example.mealerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class ClientActivity extends AppCompatActivity {
+import java.util.LinkedList;
+
+public class ClientActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button addPayment;
+    private EditText name, address, email, password, confirmPassword;
+    private EditText[] textFields;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
+
+        addPayment = (Button)findViewById(R.id.addPayment);
+        addPayment.setOnClickListener(this);
+
+        name = (EditText) findViewById(R.id.name);
+        address = (EditText) findViewById(R.id.address);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        confirmPassword = (EditText) findViewById(R.id.confirmPassword);
+
+        textFields =  new EditText[]{name,address,email, password, confirmPassword};
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.addPayment:
+                Intent intent = new Intent(this, AddPaymentMethod.class);
+                intent.putExtra("textFields", textFields);
+                startActivity(intent);
+                break;
+        }
     }
 }
