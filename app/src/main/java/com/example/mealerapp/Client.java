93f05e41057874e9d0a37069b000e57379c0248d
Lama Koleib <lkole095@uottawa.ca>
2022-10-20 12:26:58 -0400

    Added admin and UserRole; modified User to use a role

--- app/src/main/java/com/example/mealerapp/Client.java
+++ app/src/main/java/com/example/mealerapp/Client.java
@@ -3,10 +3,12 @@ package com.example.mealerapp;
 import java.util.ArrayList;
 
 public class Client extends User{
+
+
     private ArrayList<CreditCard> CreditCards;
 
-    public Client (String firstName, String lastName, String Address, String Email, String Password, CreditCard CC){
-        super(firstName, lastName, Address, Email, Password);
+    public Client (UserRole role, String firstName, String lastName, String Address, String Email, String Password, CreditCard CC){
+        super(role, firstName, lastName, Address, Email, Password);
         this.CreditCards.add(CC);
     }
 
