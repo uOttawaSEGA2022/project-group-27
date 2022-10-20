229ef22e13cb99740ac0e66a8845d540443c7287
Yassine Sami <bhstimpy@gmail.com>
2022-10-20 16:24:33 -0400

    Meal integration (initial)

--- app/src/main/java/com/example/mealerapp/Cook.java
+++ app/src/main/java/com/example/mealerapp/Cook.java
@@ -1,20 +1,20 @@
 package com.example.mealerapp;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class Cook extends User{
 
     private String description;
-    //private ArrayList<Meal> meals;
+    private ArrayList<Meal> meals;
 
     public Cook(UserRole role, String firstName, String Address, String lastName, String Email, String Password, String desc){
         super(role, firstName, lastName, Address, Email, Password);
         description = desc;
     }
-/*
+
     public void addMeal(Meal m){
         meals.add(m);
     }
-*/
+
 
 }
