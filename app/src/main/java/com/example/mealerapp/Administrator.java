6ae0a6622b546d4c4d4730ffb2425d81f32b1488
KaeBiy <jzhan609@uottawa.ca>
2022-10-27 14:26:59 -0400

    Update Administrator.java
    Can make an admin now.

--- app/src/main/java/com/example/mealerapp/Administrator.java
+++ app/src/main/java/com/example/mealerapp/Administrator.java
@@ -2,5 +2,8 @@ package com.example.mealerapp;
 
 public class Administrator extends User {
 
+    public Administrator(String Role, String firstName, String lastName, String Address, String Email, String Password){
+        super(Role, firstName, lastName, Address, Email, Password);
 
+    }
 }
