e2dfb3a25ffb30ba12605e358a4b96994bfe84f1
KaeBiy <jzhan609@uottawa.ca>
2022-10-29 18:20:39 -0400

    Added some methods to Admin

--- app/src/main/java/com/example/mealerapp/Administrator.java
+++ app/src/main/java/com/example/mealerapp/Administrator.java
@@ -1,9 +1,34 @@
 package com.example.mealerapp;
 
-public class Administrator extends User {
+import java.util.Date;
 
+public class Administrator extends User {
     public Administrator(String Role, String firstName, String lastName, String Address, String Email, String Password){
         super(Role, firstName, lastName, Address, Email, Password);
 
     }
+
+    //Yo idk how you do the login but when you login if Role == cook, then check if suspended == true.
+    //If it is, then if until is undefined then toast with like ur suspended
+    //else if Calander.getTime <= c.until then change suspended = false and login.
+
+    public void tempSus(Cook c, Date d){
+
+        c.suspended = true;
+        c.until = d;
+    }
+
+    public void permSus(Cook c){
+
+        c.suspended = true;
+    }
+    /*
+    Ill leave this for now because the complaint class isnt complete.
+    I assume complaint class will have a like delete method which will just remove it from the DB(Which i also dont know how to do
+
+    public void dismissComplaint(Complaint c){
+
+    }
+
+     */
 }
