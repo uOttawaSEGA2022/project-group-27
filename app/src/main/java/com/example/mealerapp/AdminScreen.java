15c7985d73089cd96c5d344d752bcb57d5880cf5
KaeBiy <jzhan609@uottawa.ca>
2022-11-06 23:37:28 -0500

    Finished the suspension timer for Cooks

--- app/src/main/java/com/example/mealerapp/AdminScreen.java
+++ app/src/main/java/com/example/mealerapp/AdminScreen.java
@@ -17,6 +17,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Currency;
 import java.util.List;
 
 public class AdminScreen extends AppCompatActivity {
@@ -72,7 +74,15 @@ public class AdminScreen extends AppCompatActivity {
             public void onClick(View view) {
                 DatabaseReference dR = FirebaseDatabase.getInstance().getReference("users").child(cookID);
                 dR.child("suspended").setValue(true);
-                dR.child("until").setValue(Integer.parseInt(_textUntil.toString()));
+
+                if(_textUntil.toString() == ""){
+
+                }else{
+
+                    Calendar c = Calendar.getInstance();
+                    c.add(Calendar.DAY_OF_YEAR,Integer.parseInt(_textUntil.toString());
+                    dR.child("until").setValue(c.getTime());
+                }
             }
         });
     }
