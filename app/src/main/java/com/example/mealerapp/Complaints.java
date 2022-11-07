4571ac81191f3b08931c1c3fe913ee1ef021ea84
xSui <lishilin15@gmail.com>
2022-11-06 19:18:19 -0500

    fixed

--- app/src/main/java/com/example/mealerapp/Complaints.java
+++ app/src/main/java/com/example/mealerapp/Complaints.java
@@ -8,24 +8,24 @@ import com.google.firebase.database.ValueEventListener;
 
 
 public class Complaints {
-<<<<<<< HEAD
+
     public Cook complained_cook;  //added by Carl because the activity is needed
     public String description;
     public  String _id;
 
-    public Complaints(String id, Cook target_cook, String description){
-        this._id = id;
-=======
-    private Cook complained_cook;  //added by Carl because the activity is needed
-    private String details;
 
-    public Complaints(Cook target_cook, String details){
->>>>>>> e969f3f33f542ff857d69322cb88b700bdd2de3d
+
+
+
+    public Complaints(Cook target_cook, String details, String id) {
+
         this.complained_cook = target_cook;
-        this.details = details;
+        this.description = details;
+        this._id = id;
     }
 
-<<<<<<< HEAD
+
+
     public Cook get_Cook(){ return complained_cook; }
     public String getDescription(){ return description; }
     public void setId(String id) {
@@ -35,10 +35,9 @@ public class Complaints {
         return _id;
     }
     public String getName() { return complained_cook.getFirstName() + complained_cook.getLastName(); }
-=======
-    public Cook get_Cook(){ return this.complained_cook; }
-    public String getDetails(){ return this.details; }
->>>>>>> e969f3f33f542ff857d69322cb88b700bdd2de3d
+
+
+
 
 //there should be a list of complaints that the admin can view and it is added to the database
 //with an add_complaints() method, each time a client makes a complaint, it's added here
