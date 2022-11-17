3d2e8e8c9d4e0546dd6d18d0be287b8174d9fcc5
xSui <lishilin15@gmail.com>
2022-11-17 15:23:23 -0500

    added manage menu

--- app/src/main/java/com/example/mealerapp/Activity/ManageMenuActivity.java
+++ app/src/main/java/com/example/mealerapp/Activity/ManageMenuActivity.java
@@ -3,15 +3,22 @@ package com.example.mealerapp.Activity;
 import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
-import android.widget.Toast;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.example.mealerapp.Objects.Cook;
 import com.example.mealerapp.Objects.Meal;
-import com.example.mealerapp.Objects.User;
 import com.example.mealerapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -22,6 +29,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ManageMenuActivity extends AppCompatActivity {
@@ -29,17 +40,164 @@ public class ManageMenuActivity extends AppCompatActivity {
     private List<Meal> menu;
 
     private Cook cook;
+    private ListView listView_meal;
+    private ArrayList<Meal> list_meal;
+    private Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_menu);
 
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        listView_meal = (ListView) findViewById(R.id.meal_list);
+
 
         getCook();
+        list_meal = cook.get_mealList();
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addMeals();
+            }
+        });
+
+        ArrayAdapter ad = new ArrayAdapter(this, R.layout.activity_manage_menu, list_meal);
+        listView_meal.setAdapter(ad);
+        listView_meal.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Meal target_meal =list_meal.get(i);
+                showUpdateMealDialog(target_meal);
+                return true;
+            }
+        });
+
+    }
+
+    private void addMeals(){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View detailView = inflater.inflate(R.layout.activity_meal_detail, null);
+        dialogBuilder.setView(detailView);
+
+        final EditText editName = (EditText) detailView.findViewById(R.id.txtName);
+        final EditText editCourse = (EditText) detailView.findViewById(R.id.txtCourse);
+        final EditText editCuisine = (EditText) detailView.findViewById(R.id.txtCuisine);
+        final EditText editIngredients = (EditText) detailView.findViewById(R.id.txtIngredients);
+        final EditText editAllergens = (EditText) detailView.findViewById(R.id.txtAllergens);
+        final EditText editPrice = (EditText) detailView.findViewById(R.id.txtPrice);
+        final EditText editDescription = (EditText) detailView.findViewById(R.id.txtDescription);
+        final Button btn_delete = (Button) detailView.findViewById(R.id.btnDelete);
+        final Button btn_update = (Button) detailView.findViewById(R.id.btnUpdate);
+
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editName.getText().toString().trim();
+                String course = editCourse.getText().toString().trim();
+                String cuisine = editCuisine.getText().toString().trim();
+                ArrayList<String> Ingredients = new ArrayList<String>(Arrays.asList(editIngredients.getText().toString().trim().split(", ")));
+                ArrayList<String> Allergens = new ArrayList<String>(Arrays.asList(editAllergens.getText().toString().split(", ")));
+                BigDecimal price = BigDecimal.valueOf(Double.valueOf(editPrice.getText().toString().trim()));
+                String description = editDescription.getText().toString().trim();
+
+                if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(course) && !TextUtils.isEmpty(cuisine) && !TextUtils.isEmpty(description)){
+                    ArrayList<Meal> meals = cook.get_mealList();
+                    meals.add(new Meal(name, course, cuisine, Ingredients, Allergens, price, description));
+                    b.dismiss();
+                }
+
+            }
+        });
+        btn_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                b.dismiss();
+            }
+        });
 
 
     }
+
+    private void showUpdateMealDialog(Meal meal){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View detailView =inflater.inflate(R.layout.activity_meal_detail, null);
+        dialogBuilder.setView(detailView);
+
+        final EditText editName = (EditText) detailView.findViewById(R.id.txtName);
+        final EditText editCourse = (EditText) detailView.findViewById(R.id.txtCourse);
+        final EditText editCuisine = (EditText) detailView.findViewById(R.id.txtCuisine);
+        final EditText editIngredients = (EditText) detailView.findViewById(R.id.txtIngredients);
+        final EditText editAllergens = (EditText) detailView.findViewById(R.id.txtAllergens);
+        final EditText editPrice = (EditText) detailView.findViewById(R.id.txtPrice);
+        final EditText editDescription = (EditText) detailView.findViewById(R.id.txtDescription);
+        final Button btn_delete = (Button) detailView.findViewById(R.id.btnDelete);
+        final Button btn_update = (Button) detailView.findViewById(R.id.btnUpdate);
+
+        String tmpIngredients = meal.getIngredients().toString().trim();
+        tmpIngredients.replace("[", "");
+        tmpIngredients.replace("]", "");
+        String tmpAllergens = meal.getAllergens().toString().trim();
+        tmpAllergens.replace("[", "");
+        tmpAllergens.replace("]", "");
+
+        editName.setText(meal.getName());
+        editCourse.setText(meal.getCourse());
+        editCuisine.setText(meal.getCuisine());
+        editIngredients.setText(tmpIngredients);
+        editAllergens.setText(tmpAllergens);
+        editPrice.setText(meal.getPrice().toString());
+        editDescription.setText(meal.getDescription());
+
+        dialogBuilder.setTitle(meal.getName());
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editName.getText().toString().trim();
+                String course = editCourse.getText().toString().trim();
+                String cuisine = editCuisine.getText().toString().trim();
+                ArrayList<String> Ingredients = new ArrayList<String>(Arrays.asList(editIngredients.getText().toString().trim().split(", ")));
+                ArrayList<String> Allergens = new ArrayList<String>(Arrays.asList(editAllergens.getText().toString().split(", ")));
+                BigDecimal price = BigDecimal.valueOf(Double.valueOf(editPrice.getText().toString().trim()));
+                String description = editDescription.getText().toString().trim();
+
+                if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(course) && !TextUtils.isEmpty(cuisine) && !TextUtils.isEmpty(description)){
+                    updateMeal(meal, name, course, cuisine, Ingredients, Allergens, price, description);
+                    b.dismiss();
+                }
+
+            }
+        });
+        btn_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteMeal(meal);
+                b.dismiss();
+            }
+        });
+
+
+    }
+    private void updateMeal(Meal meal, String name, String course, String cuisine, ArrayList<String> ingredients, ArrayList<String> allergens, BigDecimal price, String description){
+        ArrayList<Meal> meals = cook.get_mealList();
+        meals.set(meals.indexOf(meal), new Meal(name, course, cuisine, ingredients, allergens, price, description));
+        cook.setMeals(meals);
+    }
+    private void deleteMeal(Meal meal){
+        ArrayList<Meal> meals = cook.get_mealList();
+        meals.remove(meal);
+        cook.setMeals(meals);
+    }
+
 
     private void getCook() {
 
